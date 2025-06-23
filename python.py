1e000a8177a37165fab081a3a0192072962604a1
Harish Thampi <harishbhupeshthampi@gmail.com>
2025-06-23 15:59:01 -0400

    Py fundamentals2

--- python.py
+++ python.py
@@ -309,3 +309,218 @@ say_hello(name='Cena') # keyword arguments
 def say_hello(name = 'Kane'):  # default parameter
   print(f'hello {name}')
 say_hello()
+
+
+
+
+#1. Wrap the above code in a function called checkDriverAge(). Whenever you call this function, you will get prompted for age. 
+# Notice the benefit in having checkDriverAge() instead of copying and pasting the function everytime?
+
+
+
+def checkDriverAge():
+	age = input("What is your age?: ")
+	if int(age) < 18:
+		print("Sorry, you are too young to drive this car. Powering off")
+	elif int(age) > 18:
+			print("Powering On. Enjoy the ride!");
+	elif int(age) == 18:
+			print("Congratulations on your first year of driving. Enjoy the ride!")
+
+checkDriverAge()
+
+#2 Instead of using the input(). Now, make the checkDriverAge() function accept an argument of age, so that if you enter:
+#checkDriverAge(92);
+#it returns "Powering On. Enjoy the ride!"
+#also make it so that the default age is set to 0 if no argument is given.
+
+def checkDriverAge(age=0):
+	if int(age) < 18:
+		print("Sorry, you are too young to drive this car. Powering off")
+	elif int(age) > 18:
+			print("Powering On. Enjoy the ride!");
+	elif int(age) == 18:
+			print("Congratulations on your first year of driving. Enjoy the ride!")
+
+checkDriverAge(18);
+
+
+
+# Methods vs Functions
+# Methods are functions that are built into a data type
+# list(), print(), max(), min(),input()
+# Functions are standalone pieces of code that you can call
+# def some_random_stuff()
+
+
+# DocStrings - means the documentation of the function
+def test(a):
+    '''
+    Info: this function tests and prints param a 
+    ''' # this is the docstring
+    print(a)
+test('abx')
+
+# *args and **kwargs - means the arguments and keyword arguments
+
+def super_func(*args): # *args means the function can take any number of arguments
+  print(args)  # args is a tuple (1, 2, 3, 4, 5)
+  return sum(args)
+print(super_func(1,2,3,4,5))
+
+def super_func(**kwargs):  # **kwargs means the function can take any number of keyword arguments
+  print(kwargs)  # kwargs is a dictionary {'num1': 10, 'num2': 205}
+super_func(num1=10,num2=205)
+# Rule: params, *args, default parameters, **kwargs
+def super_func(name,*args,i='hi',**kwargs):
+  pass
+
+def highest_even(li):
+  highest_evenNumber = 0
+  for item in li:
+      if item % 2 == 0:
+          if highest_evenNumber < item:
+              highest_evenNumber = item
+  return highest_evenNumber
+
+print(highest_even([10,2,3,4,88,11]))
+
+# Scope - what variables do I have access to?
+def total():
+  total_score = 100
+  print(total_score)
+total()
+print(total_score) # error because total_score is not defined outside the function
+# order of scope - Local Scope, Parent Scope, Global Scope, Built-in - function 
+
+total = 0
+def  count():
+  global total # global keyword is used to access the global variable
+  total+=1
+  return total
+
+# nonlocal # nonlocal keyword is used to access the parent scope variable (not a global variable but outside the scope of current function)
+x = "global"
+def outer():
+  x = "local"
+  def inner():
+    nonlocal x 
+    x = "nonlocal" # this will change the value of x in outer function
+    print("inner:", x)
+  inner()
+  print("outer:", x)
+outer()
+print("global:", x)
+
+# Pure Function - it always produces the same output for the same input and it does not have any side effects 
+def sum(num1,num2): 
+  return num1 + num2 
+print(sum(1,3)) 
+
+import random
+
+def sum_random(num1,num2):  # Non Pure Function because it produces different output for the same input because of random.randint(1, 10)
+  return num1 + num2 +random.randint(1, 10)  # random.randint(1, 10) is a side effect because it is not related to the input or output of the function
+print(sum_random(1,3)) 
+
+#  map , filter, zip, reduce
+
+#  map(function, iterable) - applies a function to all the items in an iterable. It returns a new list with the results. It does not modify the original list.
+print('Map function')
+my_list = [1,2,3,4,5]
+def multiply_by2(item): 
+  return item*2
+print(list(map(multiply_by2,my_list)))  
+print(my_list)
+
+# filter(function, iterable) - applies a function to all the items in an iterable. It returns a new list with the results. It does not modify the original list. It only returns the items that return True. 
+print('Filter function')
+my_list = [1,2,3,4,5]
+def check_even(item): 
+  return item % 2 == 0
+print(list(filter(check_even,my_list)))  
+print(my_list)
+
+#  zip(iterable1, iterable2) - combines two iterables into one. It returns a new list with the results. It does not modify the original list. 
+print('Zip function')
+my_list1 = [1,2,3,4,5]
+my_list2 = ['a','b','c','d','e']
+print(list(zip(my_list1,my_list2)))
+
+ # reduce(function, iterable) - applies a function to all the items in an iterable. It returns a single value. It does not modify the original list. It is a part of the functools module.
+print('Reduce function')
+from functools import reduce # reduce is a part of the functools module
+my_list1 = [1,2,3,4,5]
+def accumulator(acc,item):  # acc is the accumulator and item is the current item in the iterable
+  # print(acc,item)
+  return acc + item  # 0 + 1 = 1, 1 + 2 = 3, 3 + 3 = 6, 6 + 4 = 10, 10 + 5 = 15
+print(reduce(accumulator,my_list,0)) # 0 is the initial value of the accumulator
+
+# list comprehension - a way to create a new list from an existing list
+print('List comprehension')
+
+my_list = []
+for char in 'hello':
+  my_list.append(char)
+print(my_list)
+
+# my_listcomprehension = [param for param in 'hello']
+my_listcomprehension = [char for char in 'hello']  # param is the variable that we are using inside the loop
+print(my_listcomprehension)
+
+my_list2 = [num for num in range(0,100)]
+my_list3 =  [num*2 for num in range(0,100)]  # we can also do some operations on the variable
+my_list4 =  [num for num in range(0,100) 
+            if num % 2 == 0]  # if condition is true then only the value is added to the list
+print(my_list2)
+print(my_list3)
+print(my_list4)
+# set comprehension - a way to create a new set from an existing set
+my_set = {char for char in 'hello'}
+print(my_set)
+my_set2 = {num for num in range(0,100)}
+print(my_set2)
+my_set3 =  {num for num in range(0,100) 
+  if num % 2 == 0}
+print(my_set3)
+#  dictionary comprehension - a way to create a new dictionary from an existing dictionary
+simple_dict = {
+  'a':1,
+  'b':2
+}
+my_dict = {key:value ** 2 for key,value in simple_dict.items()} 
+print(  my_dict)
+my_dict2 = {key:value ** 2 for key,value in simple_dict.items()
+           if value % 2 == 0} 
+print( my_dict2)
+my_dict3 = {num:num*2 for num in [1,2,3]}
+print(my_dict3)
+some_list = ['a', 'b', 'c', 'b', 'd', 'm', 'n', 'n']
+
+# duplicates1= []
+# for value in some_list:
+#     if some_list.count(value) > 1:
+#         if value not in duplicates:
+#             duplicates1.append(value)
+
+# print(duplicates1)
+
+duplicates = list({char for char in some_list
+             if some_list.count(char) > 1})
+print(duplicates)
+
+# Modules in Python are simply Python files with .py extension, which implement a set of functions. Modules are a way of organizing Python code into reusable pieces.
+
+# import - is the keyword for importing a module  - import <module_name>
+
+pycharm - is the IDE for python
+Packages in Python are a way of organizing related modules into a single package. Import <package_name>.<module_name>   
+
+Different ways of importing a module -
+import <module_name>  - import the whole module
+import <module_name> as <alias>  - import the whole module with an alias
+from <module_name> import <function_name>  - import a specific function from a module
+from <module_name> import *  - import all the functions from a module
+from <module_name> import <function_name> as <alias>  - import a specific function from a module with an alias
+
+_init__.py - is the file that makes a directory a package
